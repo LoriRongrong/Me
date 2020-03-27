fcb922716e4c1b2ca58924a7e57b734f7afe01ef
“LoriRongrong” <liurongrong2017@outlook.com>
2020-03-26 21:37:18 -0700

    Updates

---
+++ precache-manifest.f25872a7e9152bacaa5bfe1b48724aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a68acea90047fbc697fff3afae11e48",
+    "url": "/Me/index.html"
+  },
+  {
+    "revision": "32a60d514a9d0ac5f45f",
+    "url": "/Me/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "fe8a4686300ce0575c68",
+    "url": "/Me/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "32a60d514a9d0ac5f45f",
+    "url": "/Me/static/js/2.e01bdde3.chunk.js"
+  },
+  {
+    "revision": "ab7d6f2978cd1032ac3723c02d2475fb",
+    "url": "/Me/static/js/2.e01bdde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8a4686300ce0575c68",
+    "url": "/Me/static/js/main.7902be58.chunk.js"
+  },
+  {
+    "revision": "3e06403e162930b41133",
+    "url": "/Me/static/js/runtime-main.01ece0c8.js"
+  }
+]);
