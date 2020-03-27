a16e6339f5ab6631bcce14d8c9c99d5c74c60f53
“LoriRongrong” <liurongrong2017@outlook.com>
2020-03-26 19:14:09 -0700

    My Personal Website

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+class Portfolio extends Component {
+  render() {
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function(project) {
+        var projectImage = "images/portfolio/" + project.image;
+        return (
+          <div
+            key={project.title}
+            style={{ widht: 600, heght: 650, backgroundColor: "#25274d" }}
+          >
+            <img
+              src={projectImage}
+              alt={project.title}
+              style={{ width: 500, heght: 550 }}
+            />
+          </div>
+        );
+      });
+    }
+
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapse">
+            {/* <div className="center"> */}
+            <h1>My Artwork</h1>
+            <Carousel showStatus={false} infiniteLoop={true}>
+              {projects}
+            </Carousel>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
